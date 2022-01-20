f72a88e4428273d7b97343044a818c00ed1cf25b
plarrat <larrat.philippe@lessonsharing.fr>
2022-01-20 12:49:58 +0100

    initialisation projet

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppNavbar from './Components/AppNavbar/AppNavbar'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <AppNavbar></AppNavbar>
+      </header>
+    </div>
+  )
+}
+
+export default App
